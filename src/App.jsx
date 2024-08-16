ae4645a25b031586af14c9117f81ce808f589c01
Raúl Peña <25549072+satara2358@users.noreply.github.com>
2024-08-16 18:21:44 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,39 @@ function App() {
 
 
   return (
-    <div className="bg-slate-600 items-center">Demo Api xxxxxxx </div>
+    <div >
+      <h1 >Search Película</h1>
+      <form 
+        onSubmit={handleSubmit}
+        className="text-center">
+        <input
+          type="text"
+          placeholder="Buscador de películas"
+          value={pelicula}
+          onChange={handleInputChange}
+          className=""
+        />
+        <button
+          type="submit"
+          className=""
+        >
+          Buscar
+        </button>
+      </form>
+      <div className="">
+        {movies.map((mov) => (
+          <div  key={mov.id} className="">
+            <img 
+              src={`https://image.tmdb.org/t/p/w500${mov.poster_path}`} 
+              alt={mov.title} 
+              className=""
+            />
+            <h1>{mov.title}</h1>
+            <p>{mov.overview}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
